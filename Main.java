6e83ec379b275958ed901baa3b94a7026036be4b
Max Chong <127365579+MaxChong1@users.noreply.github.com>
2024-09-11 22:25:57 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Main {
+public class HappyOrSad {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("happyorsad.txt");
         Scanner scan = new Scanner(file);
